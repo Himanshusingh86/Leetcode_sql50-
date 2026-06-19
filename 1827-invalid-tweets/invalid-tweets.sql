505805fe1f31f480864a6b53a4c1141c0ae518fb
Himanshu Singh <himanshusingh7679@gmail.com>
2026-06-19 23:30:39 +0530

    Time: 615 ms (52.90%) | Memory: 0B (100.00%) - LeetSync

--- 1827-invalid-tweets/invalid-tweets.sql
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select tweet_id 
-from tweets 
-where length (content)>15
+ # Write your MySQL query statement below
+SELECT tweet_id
+FROM tweets 
+where length(content) >15;
